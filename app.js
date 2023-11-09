154a3e623108914fbd3cf4805863d5a0ae6d8429
soomro12583 <nadeemkhanasf909@gmail.com>
2023-11-09 07:45:52 +0500

    pectice

---
+++ app.js
@@ -0,0 +1,12 @@
+const optionMenu =document.querySelector(".select-menu"),
+    selectBtn = optionMenu.querySelector(".select-btn"),
+    options = optionMenu.querySelectorAll(".option"),
+    sBtn_text = optionMenu.querySelector(".sBtn_text");
+
+    options.forEach(Option =>{
+        Option.addEventListener("click", ( =>{
+            let selectedOption = Option.querySelector(".option-text");
+
+        })
+        console.log(Option)
+    })
